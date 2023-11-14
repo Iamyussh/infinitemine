4b15d5bed8b5727ab35218bc7f809cfd62971817
Iamyussh <101572189+Iamyussh@users.noreply.github.com>
2023-11-14 20:56:18 +0530

    Add files via upload

---
+++ Day47/EjbStateFulljsf/src/main/java/com/java/ejb/EjbImpl.java
@@ -0,0 +1,11 @@
+package com.java.ejb;
+
+import java.util.List;
+
+public class EjbImpl {
+	
+	
+		public void sayHelloejb() {
+		HelloBeanRemote remote = StateFullClient.lookupRemoteStateFullHello()
+		}
+}
